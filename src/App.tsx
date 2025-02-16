78557425eb2710d542a4ef36515cf10fb6809ce3
Mostafizur Rahman <mostafizurhimself@gmail.com>
2025-02-16 20:46:20 +0600

    feat: react query pagination

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       throwOnError: true,
+
+      // @ts-ignore
       suspense: true,
       retry: false,
       // enabled: false,
@@ -23,20 +25,17 @@ function App() {
         <QueryClientProvider client={queryClient}>
           <h1 className="text-2xl font-bold mb-5">Products</h1>
 
-          <div className="grid grid-cols-3 gap-4">
-            <ErrorBoundary
-              fallback={
-                <div className="text-red-500 px-4 py-2 border border-dashed rounded">
-                  Something went wrong!
-                </div>
-              }
-            >
-              <Suspense fallback={<div>Loading From React Query...</div>}>
-                <ProductList />
-                <ProductList />
-              </Suspense>
-            </ErrorBoundary>
-          </div>
+          <ErrorBoundary
+            fallback={
+              <div className="text-red-500 px-4 py-2 border border-dashed rounded">
+                Something went wrong!
+              </div>
+            }
+          >
+            <Suspense fallback={<div>Loading From React Query...</div>}>
+              <ProductList />
+            </Suspense>
+          </ErrorBoundary>
         </QueryClientProvider>
       </div>
     </div>
--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,51 +1,90 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { FC, useEffect, useState, use } from "react";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { FC, useState } from "react";
 
 type Product = {
   id: number;
   title: string;
   price: number;
   description: string;
-  category: string;
-  image: string;
+  category: {
+    id: number;
+    name: string;
+    image: string;
+  };
+  images: string[];
 };
 
 const ProductList: FC = () => {
+  const [page, setPage] = useState(1);
+
+  const limit = 12;
+
+  const offset = (page - 1) * limit;
+
+  const hasPreviousData = offset > 0;
+
+  const hasNextData = offset + limit < 100;
+
   const {
     data: products,
     // isLoading,
     // isError,
   } = useQuery<Product[]>({
-    queryKey: ["products"],
-    queryFn: () => {
-      return fetch("https://fakestoreapi.com/products adfaasd;f").then((res) =>
-        res.json()
-      );
+    queryKey: ["products", offset, limit],
+    queryFn: ({ queryKey: [_, offset, limit] }) => {
+      return fetch(
+        `https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`
+      ).then((res) => res.json());
     },
+    placeholderData: keepPreviousData,
   });
 
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
+  const goToNextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
-  // if (isError) {
-  //   return <div className="text-red-500">Error fetching products</div>;
-  // }
+  const goToPrevPage = () => {
+    setPage((prevPage) => prevPage - 1);
+  };
 
   return (
     <>
-      {products?.map((product: Product) => (
-        <div key={product.id} className="bg-gray-100 p-5 rounded-lg">
-          <img
-            src={product.image}
-            alt={product.title}
-            className="w-full h-40 object-cover"
-          />
-          <h2 className="text-lg font-semibold mt-2">{product.title}</h2>
-          <p className="text-sm text-gray-500 mt-2">{product.description}</p>
-          <p className="text-lg font-semibold mt-2">${product.price}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-3 gap-4">
+        {products?.map((product: Product) => (
+          <div key={product.id} className="bg-gray-100 p-5 rounded-lg">
+            <img
+              src={product.images[0]}
+              alt={product.title}
+              className="w-full h-40 object-cover"
+            />
+            <h2 className="text-base font-semibold mt-2">{product.title}</h2>
+            <div className="flex items-center justify-between mt-2">
+              <p className="text-lg font-semibold mt-2">${product.price}</p>
+              <button className="bg-emerald-500 text-sm text-emerald-50 px-4 py-2 rounded cursor-pointer hover:bg-emarald-600">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex items-center gap-2 justify-center mt-6 w-full pb-20">
+        <button
+          className="bg-emerald-500 text-sm text-emerald-50 px-4 py-2 rounded cursor-pointer hover:bg-emarald-600 disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={goToPrevPage}
+          disabled={!hasPreviousData}
+        >
+          Prev
+        </button>
+
+        <button
+          className="bg-emerald-500 text-sm text-emerald-50 px-4 py-2 rounded cursor-pointer hover:bg-emarald-600 disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={goToNextPage}
+          disabled={!hasNextData}
+        >
+          Next
+        </button>
+      </div>
     </>
   );
 };
